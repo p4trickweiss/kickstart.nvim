a4d6e4dab949127b7d9192a9fce55d1d2cd2c4f8
p4trickweiss <p4trick.weiss.dev@gmail.com>
2026-06-10 16:52:52 +0200

    Format on save

--- init.lua
+++ init.lua
@@ -794,8 +794,13 @@ do
     format_on_save = function(bufnr)
       -- You can specify filetypes to autoformat on save here:
       local enabled_filetypes = {
-        -- lua = true,
-        -- python = true,
+        javascript = true,
+        typescript = true,
+        css = true,
+        html = true,
+        json = true,
+        yaml = true,
+        markdown = true,
       }
       if enabled_filetypes[vim.bo[bufnr].filetype] then
         return { timeout_ms = 500 }
@@ -810,11 +815,16 @@ do
     formatters_by_ft = {
       lua = { 'stylua' },
       go = { 'gofmt' },
-      typescript = { 'prettierd', 'prettier', stop_after_first = true },
-      javascript = { 'prettierd', 'prettier', stop_after_first = true },
-      -- rust = { 'rustfmt' },
+      typescript = { 'prettier' },
+      javascript = { 'prettier' },
+      json = { 'prettier' },
+      markdown = { 'prettier' },
+
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
+      --
+      -- You can use 'stop_after_first' to run the first available formatter from the list
+      -- javascript = { "prettierd", "prettier", stop_after_first = true },
     },
   }
 
